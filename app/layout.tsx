20d86d1f1b248bcb852d268dcfa3da4ffe3d6516
ismailAchibat <ismailachibat4@gmail.com>
2025-11-05 10:57:52 +0100

    sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <SidebarProvider>
+      <AppSidebar />
+      <html>
+        <body>
+          <main>
+            {children}
+          </main>
+        </body>
+      </html>
+    </SidebarProvider>
   );
 }
